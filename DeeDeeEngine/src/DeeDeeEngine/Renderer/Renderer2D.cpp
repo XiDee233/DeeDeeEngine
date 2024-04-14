4371b32269b40ffba08ca4f0be2fb978ede0ddc4
suyb <suyb@zhuziplay.com>
2024-04-14 23:53:24 +0800

    2d renderer transform

--- DeeDeeEngine/src/DeeDeeEngine/Renderer/Renderer2D.cpp
+++ DeeDeeEngine/src/DeeDeeEngine/Renderer/Renderer2D.cpp
@@ -3,6 +3,8 @@
 #include "VertexArray.h"
 #include "Shader.h"
 #include <Platform\OpenGL\OpenGLShader.h>
+#include <glm/gtc/matrix_transform.hpp>
+
 namespace DeeDeeEngine {
 
 	struct Renderer2DStorage {
@@ -46,24 +48,27 @@ namespace DeeDeeEngine {
 	}
 	void Renderer2D::BeginScene(const OrthographicCamera& camera)
 	{
-		std::dynamic_pointer_cast<OpenGLShader>(m_Data->FlatColorShader)->Bind();
-		std::dynamic_pointer_cast<OpenGLShader>(m_Data->FlatColorShader)->UploadUniformMat4("u_ViewProjection",camera.GetViewProjectionMartix());
-		std::dynamic_pointer_cast<OpenGLShader>(m_Data->FlatColorShader)->UploadUniformMat4("u_Transform", glm::mat4(1.0f));
+		m_Data->FlatColorShader->Bind();
+		m_Data->FlatColorShader->SetMat4("u_ViewProjection",camera.GetViewProjectionMartix());
 
 	}
 	void Renderer2D::EndScene()
 	{
 	}
-	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 	{
 
-		std::dynamic_pointer_cast<OpenGLShader>(m_Data->FlatColorShader)->Bind();
-		std::dynamic_pointer_cast<OpenGLShader>(m_Data->FlatColorShader)->UploadUniformFloat4("u_Color", color);
+		m_Data->FlatColorShader->Bind();
+		m_Data->FlatColorShader->SetFloat4("u_Color", color);
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x,size.y,1.0f });
+		m_Data->FlatColorShader->SetMat4("u_Transform", transform);
+
 
 		m_Data->QuadVertexArray->Bind();
 		RenderCommand::DrawIndexed(m_Data->QuadVertexArray);
 	}
-	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
+	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
 	{
 		DrawQuad({ position.x,position.y,0.0f }, size, color);
 	}
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -2,7 +2,6 @@
 #include "imgui\imgui.h"
 #include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
 #include <glm/gtc/type_ptr.hpp>
-#include <Platform\OpenGL\OpenGLShader.h>
 
 Sandbox2D::Sandbox2D()
 	:Layer("Sandbox2D"),m_CameraController(1280.0f/720.0f)
@@ -31,6 +30,8 @@ void Sandbox2D::OnUpdate(DeeDeeEngine::Timestep ts)
 
 	DeeDeeEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
 	DeeDeeEngine::Renderer2D::DrawQuad({ 0.0f,0.0f }, { 1.0f,1.0f }, { 0.8f,0.2f,0.3f,1.0f });
+	DeeDeeEngine::Renderer2D::DrawQuad({ 0.5f,-0.5f }, { 0.5f,0.75f }, { 0.2f,0.3f,0.8f,1.0f });
+
 
 
 	//std::dynamic_pointer_cast<DeeDeeEngine::OpenGLShader>(m_FlatColorShader)->Bind();
