c5fd6bd4ae54c6b670aa46f7f2e0d664ae618d70
suyb <suyb@zhuziplay.com>
2024-05-01 22:43:38 +0800

    transformUi

--- Dee_Editor/src/EditorLayer.cpp
+++ Dee_Editor/src/EditorLayer.cpp
@@ -67,8 +67,8 @@ namespace DeeDeeEngine {
 		class CameraController:public ScriptableEntity {
 		public:
 			virtual void OnCreate()override {
-				auto& transform = GetComponent<TransformComponent>().Transform;
-				transform[3][0] = rand() % 10 - 5.0f;
+				auto& translation = GetComponent<TransformComponent>().Translation;
+				translation.x = rand() % 10 - 5.0f;
 
 			}
 
@@ -77,17 +77,17 @@ namespace DeeDeeEngine {
 			}
 
 			virtual void OnUpdate(Timestep ts)override {
-				auto& transform = GetComponent<TransformComponent>().Transform;
+				auto& translation = GetComponent<TransformComponent>().Translation;
 				float speed = 5.0f;
 
 				if (Input::IsKeyPressed(DEE_KEY_A))
-					transform[3][0] -= speed * ts;
+					translation.x -= speed * ts;
 				if (Input::IsKeyPressed(DEE_KEY_D))
-					transform[3][0] += speed * ts;
+					translation.x += speed * ts;
 				if (Input::IsKeyPressed(DEE_KEY_W))
-					transform[3][1] += speed * ts;
+					translation.y += speed * ts;
 				if (Input::IsKeyPressed(DEE_KEY_S))
-					transform[3][1] -= speed * ts;
+					translation.y -= speed * ts;
 			}
 		};
 		m_CameraEntity.AddComponent<NativeScriptComponent>().Bind<CameraController>();
